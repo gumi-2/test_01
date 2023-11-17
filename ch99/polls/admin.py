f8436320aac518aaf00890b3a421bf6d7d0eed16
lhr <hyunggok@gumi.ac.kr>
2023-11-17 09:44:06 +0900

    base_site.html 수정된 파일

--- ch99/polls/admin.py
+++ ch99/polls/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from polls.models import Question, Choice
 
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+#class ChoiceInline(admin.StackedInline):
     model = Choice
     extra = 2
     
@@ -12,6 +13,9 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date Information',{'fields':['pub_date'], 'classes':['collapse']}),
         ]
     inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['question_text']
 #    fields = ['pub_date', 'question_text']
 
 admin.site.register(Question, QuestionAdmin)
